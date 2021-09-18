967e86b80a1e29caa20373f2d27980193eda852c
Rafael Pereira de Oliveira <rafael.oliveira4@alunos.unis.edu.br>
2021-09-17 22:11:31 -0300

    Exemplo de interligação dinâmica e Condicional

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 var app = new Vue({
     el: '#app',
     data: {
-        mensagem: 'Olá Vue JS',
+        mensagem: 'Olá Vue JS'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        mensagem: 'Você carregou esta página em ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
     }
 })
